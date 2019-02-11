c6461e8737f18380e2c0aab1b85bdf33c5ba9344
root <root@rtc.netbriefings.com>
2019-02-11 00:20:21 -0500

    Updates

---
+++ precache-manifest.7d522083816e23601355f1fcd68d4ae0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/videojsplayer_test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a0f07bb059c012e0c1a4",
+    "url": "/videojsplayer_test/static/js/runtime~main.a0f07bb0.js"
+  },
+  {
+    "revision": "a34bbdc1a70404c10fd6",
+    "url": "/videojsplayer_test/static/js/main.a34bbdc1.chunk.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/videojsplayer_test/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "a34bbdc1a70404c10fd6",
+    "url": "/videojsplayer_test/static/css/main.ddcd58dc.chunk.css"
+  },
+  {
+    "revision": "c2de09429d84901b1d3ce91b92bfa3c6",
+    "url": "/videojsplayer_test/index.html"
+  }
+];
